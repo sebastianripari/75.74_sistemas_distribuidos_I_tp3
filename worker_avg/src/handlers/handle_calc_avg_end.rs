4c7147462147d009f9f0af0ba5507d2cc9e7d18c
Sebastian Ripari <sebastiandripari@gmail.com>
2022-06-11 05:15:29 -0300

    avg using middleware

--- worker_avg/src/handlers/handle_calc_avg_end.rs
+++ worker_avg/src/handlers/handle_calc_avg_end.rs
@@ -1,12 +1,17 @@
 use amiquip::{Exchange};
 
 use crate::{
-    utils::{logger::Logger, middleware::middleware_send_msg}, constants::queues::{AVG_TO_FILTER_SCORE, QUEUE_TO_CLIENT},
+    utils::{logger::Logger, middleware::middleware_send_msg}, constants::queues::{AVG_TO_FILTER_SCORE, QUEUE_TO_CLIENT}, messages::outbound::message_client::Data,
 };
 
 fn publish_score_avg(exchange: &Exchange, score_avg: f32) {
     middleware_send_msg(exchange, &score_avg, AVG_TO_FILTER_SCORE);
-    middleware_send_msg(exchange, &score_avg, QUEUE_TO_CLIENT)
+
+    let payload = Data{
+        key: "posts_score__avg".to_string(),
+        value: score_avg.to_string()
+    };
+    middleware_send_msg(exchange, &payload, QUEUE_TO_CLIENT)
 }
 
 pub fn handle_calc_avg_end(exchange: &Exchange, logger: &Logger, score_sum: u64, score_count: usize) {
