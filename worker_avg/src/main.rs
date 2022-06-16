bf43a070b1a3749790e9a3a0293e32c0d6c3ac67
Sebastian Ripari <sebastiandripari@gmail.com>
2022-06-16 20:27:12 -0300

    connection close ok and missing break

--- worker_avg/src/main.rs
+++ worker_avg/src/main.rs
@@ -73,11 +73,15 @@ fn main() {
                     break;
                 }
             }
-            _ => {}
+            _ => {
+                break;
+            }
         }
     }
 
-    connection.close().unwrap();
+    if let Ok(_) = connection.close() {
+        logger.info("connection closed".to_string());
+    }
 
     logger.info("shutdown".to_string());
 }
