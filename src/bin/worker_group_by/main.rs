3cd6956a9b2a6fb331fe62858eecf598d1c2346f
Sebastian Ripari <sebastiandripari@gmail.com>
2022-06-21 22:36:02 -0300

    improve loop #1

--- src/bin/worker_group_by/main.rs
+++ src/bin/worker_group_by/main.rs
@@ -1,9 +1,25 @@
 use std::collections::HashMap;
 
 use amiquip::ConsumerMessage;
-use handlers::{handle_posts::{self, handle_posts}, handle_comments::{self, handle_comments}};
-use messages::{data_post_url::DataPostUrl, data_comment_sentiment::DataCommentSentiment, data_best_url::DataBestUrl};
-use reddit_meme_analyzer::commons::{utils::{logger::logger_create, middleware::{middleware_connect, middleware_create_channel, middleware_declare_queue, middleware_create_consumer, middleware_create_exchange, Message, middleware_consumer_end, middleware_send_msg, MESSAGE_OPCODE_END, MESSAGE_OPCODE_NORMAL}}, constants::queues::{QUEUE_POSTS_TO_GROUP_BY, QUEUE_COMMENTS_TO_GROUP_BY, QUEUE_TO_CLIENT}};
+use handlers::{
+    handle_comments::{handle_comments},
+    handle_posts::{handle_posts},
+};
+use messages::{
+    data_best_url::DataBestUrl, data_comment_sentiment::DataCommentSentiment,
+    data_post_url::DataPostUrl,
+};
+use reddit_meme_analyzer::commons::{
+    constants::queues::{QUEUE_COMMENTS_TO_GROUP_BY, QUEUE_POSTS_TO_GROUP_BY, QUEUE_TO_CLIENT},
+    utils::{
+        logger::logger_create,
+        middleware::{
+            middleware_connect, middleware_consumer_end, middleware_create_channel,
+            middleware_create_consumer, middleware_create_exchange, middleware_declare_queue,
+            middleware_send_msg, Message, MESSAGE_OPCODE_END, MESSAGE_OPCODE_NORMAL,
+        },
+    },
+};
 
 mod handlers;
 mod messages;
@@ -22,79 +38,65 @@ fn main() {
 
     let mut end = false;
     let mut n_end = 0;
-    let mut n_posts_processed = 0;
+    let mut n_processed = 0;
     let mut posts = HashMap::new();
+
     for message in consumer_posts.receiver().iter() {
-        match message {
-            ConsumerMessage::Delivery(delivery) => {
-                let body = String::from_utf8_lossy(&delivery.body);
-                let msg: Message<Vec<DataPostUrl>> = serde_json::from_str(&body).unwrap();
-                let opcode = msg.opcode;
-                let payload = msg.payload;
+        if let ConsumerMessage::Delivery(delivery) = message {
+            let body = String::from_utf8_lossy(&delivery.body);
+            let msg: Message<Vec<DataPostUrl>> = serde_json::from_str(&body).unwrap();
+            let opcode = msg.opcode;
+            let payload = msg.payload;
 
-                match opcode {
-                    MESSAGE_OPCODE_END => {
-                        end = middleware_consumer_end(&mut n_end, &exchange, [].to_vec(), 0);
-                    }
-                    MESSAGE_OPCODE_NORMAL => {
-                        handle_posts(
-                            payload.unwrap(),
-                            &mut n_posts_processed,
-                            &logger,
-                            &mut posts,
-                        );
-                    }
-                    _ => {}
+            match opcode {
+                MESSAGE_OPCODE_END => {
+                    end = middleware_consumer_end(&mut n_end, &exchange, [].to_vec(), 0);
                 }
-
-                consumer_posts.ack(delivery).unwrap();
-
-                if end {
-                    break;
+                MESSAGE_OPCODE_NORMAL => {
+                    handle_posts(payload.unwrap(), &mut n_processed, &logger, &mut posts);
                 }
+                _ => {}
             }
-            _ => {
+
+            consumer_posts.ack(delivery).unwrap();
+
+            if end {
                 break;
             }
         }
     }
 
-    let mut n_comments_processed = 0;
     let mut comments = HashMap::new();
+    n_processed = 0;
     end = false;
     n_end = 0;
 
     for message in consumer_comments.receiver().iter() {
-        match message {
-            ConsumerMessage::Delivery(delivery) => {
-                let body = String::from_utf8_lossy(&delivery.body);
-                let msg: Message<Vec<DataCommentSentiment>> = serde_json::from_str(&body).unwrap();
-                let opcode = msg.opcode;
-                let payload = msg.payload;
+        if let ConsumerMessage::Delivery(delivery) = message {
+            let body = String::from_utf8_lossy(&delivery.body);
+            let msg: Message<Vec<DataCommentSentiment>> = serde_json::from_str(&body).unwrap();
+            let opcode = msg.opcode;
+            let payload = msg.payload;
 
-                match opcode {
-                    MESSAGE_OPCODE_END => {
-                        end = middleware_consumer_end(&mut n_end, &exchange, [].to_vec(), 1);
-                    }
-                    MESSAGE_OPCODE_NORMAL => {
-                        handle_comments(
-                            payload.unwrap(),
-                            &mut n_comments_processed,
-                            &logger,
-                            &posts,
-                            &mut comments,
-                        );
-                    }
-                    _ => {}
+            match opcode {
+                MESSAGE_OPCODE_END => {
+                    end = middleware_consumer_end(&mut n_end, &exchange, [].to_vec(), 1);
                 }
-
-                consumer_comments.ack(delivery).unwrap();
-
-                if end {
-                    break;
+                MESSAGE_OPCODE_NORMAL => {
+                    handle_comments(
+                        payload.unwrap(),
+                        &mut n_processed,
+                        &logger,
+                        &posts,
+                        &mut comments,
+                    );
                 }
+                _ => {}
             }
-            _ => {
+
+            consumer_comments.ack(delivery).unwrap();
+
+            if end {
                 break;
             }
         }
@@ -108,7 +110,7 @@ fn main() {
     });
 
     if let Some(max_value) = max {
-        let url = max_value.1.2.to_string();
+        let url = max_value.1 .2.to_string();
         let payload = DataBestUrl {
             key: "meme_with_best_sentiment".to_string(),
             value: url,
